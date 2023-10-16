01142507ea5c5470c8e0c0b1d12aaaba932b0444
EsparzaRSE <esparzarse@gmail.com>
2023-10-16 13:03:08 +0200

    Variblaes no inicializadas

---
+++ video7/variablesNoinicializadas.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+
+    int x;      //mala práctica
+    int y [9];  //ok
+    int z {};   //ok si quieres que no tenga valor
+
+    //Nada reseñable en este vídeo, solo el comportamiento indefinido que pueden producir las variables sin inicializar.
+}
