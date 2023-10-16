4a57c651589c0c56882865b850bfdda465e4676d
EsparzaRSE <esparzarse@gmail.com>
2023-10-16 10:43:56 +0200

    comentarios y sintaxis

---
+++ video3/comentarios.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+
+int main(){
+
+    printf("Hola mundo");
+
+    //Se puede utilizar printf con el cstdio
+    /*
+      Al final da lo mismo lo que uses pero supongo que queda más pro usar cout <<
+    */
+
+}
