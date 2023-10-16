2f8f810d5ed36220d6f1ad1719aaa443d7202c38
EsparzaRSE <esparzarse@gmail.com>
2023-10-16 12:43:24 +0200

    Librería iostream

---
+++ video6/libreriaIOstream.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    int x {};
+
+    std::cout << "Introduce un número a continuación: ";
+    std::cin >> x;
+    std::cout << "Has introducido el número " << x << "\n";
+    return 0;      
+
+}
