3cf103e2a507533b356afb9f13a180e3b1e4a6ae
EsparzaRSE <esparzarse@gmail.com>
2023-10-17 15:09:30 +0200

    Header guards

---
+++ video20/headerguard.h
@@ -0,0 +1,7 @@
+#pragma once          //hace lo mismo que los define condicionales
+
+#ifndef OTROHEADER_H  //se suele poner el mismo nombre que el header en cuestión, sustituyendolo por mayúscula y el espacio por un _
+#define OTROHEADER_H
+
+#endif 
+
