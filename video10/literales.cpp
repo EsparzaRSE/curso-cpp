2c834d6defc6a0f6a1850bd2f367a803093599e1
EsparzaRSE <esparzarse@gmail.com>
2023-10-17 08:26:50 +0200

    Literales, operadores y expresiones

---
+++ video10/literales.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    int x {5 + 3};  //la expresión es 5+3 y forma parte de una declaración int x {}; que es la que realmente lleva el punto y coma ;
+
+
+    int y {};
+
+    y = 3; //Declaración expresión válida. La declaración es el ; y la expresión el resto.
+
+}
