ee34136006f62b1b11017ca1fbe42afa606de9c7
EsparzaRSE <esparzarse@gmail.com>
2023-10-18 09:12:50 +0200

    Tamaño de los objetos y sizeof

---
+++ video22/TamanioObjetos.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "El tamaño del tipo char es:\t" << sizeof(char) << " bytes\n";
+    std::cout << "El tamaño del tipo char es:\t" << sizeof(int) << " bytes\n";
+    return 0; 
+}
