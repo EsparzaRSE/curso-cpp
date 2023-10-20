22611f25337e59f5acee0b61ce0ee7adf477ab5f
EsparzaRSE <esparzarse@gmail.com>
2023-10-20 09:53:41 +0200

    Variables inline C++17

---
+++ videos26_50/video44/constantes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+namespace constantes{
+
+    inline constexpr double pi { 3.14159 };
+}
+
+#endif
---
+++ videos26_50/video44/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "constantes.h"
+
+int main(){
+
+    std::cout << "Imprimiendo variables inline: " << constantes::pi << "\n";
+
+    return 0;
+}
