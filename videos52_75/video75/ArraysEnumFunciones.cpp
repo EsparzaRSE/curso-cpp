d8734e680e275127c6e94a28709a57ae687418a6
EsparzaRSE <esparzarse@gmail.com>
2023-10-25 10:38:43 +0200

    Arrays y enum

---
+++ videos52_75/video75/ArraysEnumFunciones.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+enum NombreEstudiantes{
+
+    Pedro, Juan, Miguel, Alberto, max_estudiantes
+    //con max_estudiantes, si añadimos otro estudiante pasa a ser N+1, por lo que se actualiza solo el tamaño del array
+};
+
+int main(){
+
+    int notaEstudiante[max_estudiantes]{0,1,2,3}; // Se pueden inicializar así
+    int idEstudiante[]{}; // no tengo claro que hace esto, ya que si uso idEstudiante[8]; devuelve sin problemas el valor
+
+    std::cout << "Imprimiendo el array 0 y 1: " << notaEstudiante[0] << " " << notaEstudiante[Miguel] << "\n";
+    std::cout << idEstudiante[0];
+}
