9269eb07b373ce8a6a970a5d4abedaae6f532444
EsparzaRSE <esparzarse@gmail.com>
2023-10-25 09:51:08 +0200

    Arrays fijos

---
+++ videos52_75/video74/Arrays.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+
+    int notaEstudiante[25]{};  // Se declara como una variable agregando el n* de elementos que tendrá el array entre corchetes.
+
+    std::cout << "Imprimiendo el array 0 y 1: " << notaEstudiante[0] << " " << notaEstudiante[1] << "\n";
+}
