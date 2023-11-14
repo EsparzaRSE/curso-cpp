ecc10c66bc62c9b9d8c1ef6a192534636c50b68e
EsparzaRSE <esparzarse@gmail.com>
2023-11-14 15:47:25 +0100

    Vídeos 104 a 108

---
+++ videos99_127/video108/forEach.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+
+int main(){
+
+    std::vector vector{ "Pepe", "Juan", "Marta", "Bea" };
+    
+    for (const auto& elemento : vector){
+        //lo que sea
+    }
+}
--- videos99_127/video99/structs.cpp
+++ videos99_127/video99/structs.cpp
@@ -1,10 +1,11 @@
-#include <string>
-
 struct Alumno{
-    std::string nombre{};
+
     int id{};
     int edad{};
+    int tutorID{};
+
 };
 
-Alumno juan{};
-Alumno marta;
+Alumno juan = {1, 17, 3}; // Por copia-lista con igual y llaves
+//Alumno marta (2, 17, 2); // Directa usando lista entre paréntesis (C++ 20)
+Alumno pepe {3, 18, 1}; // Lista o uniforme entre llaves (preferida)
